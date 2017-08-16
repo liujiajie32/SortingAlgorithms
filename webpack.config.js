261d03f223b2521a58452ea5ff0cb7564598f0d3
liujiajie32 <liujiajie32@gmail.com>
2017-08-16 23:15:25 +0800

    [201708016]add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,24 @@ module.exports = {
       './src/Utils/GetRandomArray.js'
     ]
   },
-
   output: {
     path: __dirname + '/dist',
     filename: '[name].js'
+  },
+  module: {
+    loaders:[{
+      test:/\.js$/,
+      loader: 'babel-loader',
+      exclude:"/node_modules/",
+      include: __dirname +"/dist",
+      query: {presets: ['es2015', 'stage-0'], plugins:[]}
+    }]
+  },
+
+  resolve:{
+    extensions:['.js','.jsx']
   }
+
+
+
 }
